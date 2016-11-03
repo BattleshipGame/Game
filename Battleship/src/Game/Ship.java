c6f69f38b3d9cfeba2e41ab0cc342745fd743e8e
Maurice Ajluni <ajluni39@gmail.com>
2016-11-03 10:33:30 -0700

    Ship class defined

--- Battleship/src/Game/Ship.java
+++ Battleship/src/Game/Ship.java
@@ -2,5 +2,33 @@ package Game;
 
 public class Ship
 {
+    int size;
+    Point[] coordinates;
+    int[] hits;
     
+    public Ship(int size, Point[] points)
+    {
+        this.size = size;
+        coordinates = points;
+        hits = new int[size];
+    }
+    
+    public boolean isSunk()
+    {
+        for(int i : hits)
+            if(i == 0)
+                return false;
+        
+        return true;
+    }
+    
+    public int getSize()
+    {
+        return size;
+    }
+    
+    public Point[] getCoordinates()
+    {
+        return coordinates;
+    }
 }
